a0df6ccf284c034543da1f6098527402ae25fda5
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-11-28 08:44:56 -0300

    url do site

--- url.php
+++ url.php
@@ -4,5 +4,5 @@ function url(string $url): string
 {
     $srevidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
     $ambiente = ($srevidor == 'localhostc' ? URL_DESENVOLVIMENTO : URL_PRODUCAO);
-    return $ambiente.$url;
+    return $ambiente."/".$url;
 }
