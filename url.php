a22b98696a9c900f0c107168e1948a461f080e6a
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-11-28 08:41:01 -0300

    url do site

---
+++ url.php
@@ -0,0 +1,8 @@
+<?php 
+
+function url(string $url): string
+{
+    $srevidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
+    $ambiente = ($srevidor == 'localhostc' ? URL_DESENVOLVIMENTO : URL_PRODUCAO);
+    return $ambiente.$url;
+}
