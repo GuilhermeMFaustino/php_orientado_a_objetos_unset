f47c02eed453d4280ff94c112522ebcff6092002
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-12-03 08:18:24 -0300

    Valida CPF

--- validaCpf.php
+++ validaCpf.php
@@ -1,10 +1,8 @@
 <?php
-
-
-$cpf = '529.952.247-25';
-
-function validaCPF($cpf): bool
+function validaCPF($cpf)
 {
+
+
     $cpf = preg_replace('/[^0-9]/', "", $cpf);
     $digitoA = 0;
     $digitoB = 0;
@@ -12,12 +10,17 @@ function validaCPF($cpf): bool
     for ($i = 0, $x = 10; $i <= 8; $i++, $x--) {
         $digitoA += $cpf[$i] * $x;
     }
-    $digito1 = ($digitoA * 10) % 11;
     //echo $digito1;
     for ($a = 0, $b = 11; $a <= 9; $a++, $b--) {
         // echo $cpf[$a]." x ".$b ."= "."<br>";
+        if (str_repeat($a, 11) == $cpf) {
+            return false;
+           // return;
+        }
+        
         $digitoB += $cpf[$a] * $b;
     }
+    $digito1 = ($digitoA * 10) % 11;
     $segundo = ($digitoB * 10) % 11;
     //echo $segundo;
     if ($digito1 != $cpf[9] || $segundo != $cpf[10]) {
@@ -27,4 +30,5 @@ function validaCPF($cpf): bool
     }
 }
 
+$cpf = '058.973.224-22';
 var_dump(validaCpf($cpf));
